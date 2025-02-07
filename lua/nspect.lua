362970cef5d7c5261bf9216b703812167020ce5e
Blake Astley <astley92@hotmail.com>
2025-02-07 13:28:16 +1100

    Refactoring

--- lua/nspect.lua
+++ lua/nspect.lua
@@ -6,12 +6,10 @@ local M = {}
 
 M.setup = function()
   M.plugin_root = debug.getinfo(1, "S").source:sub(2):match("(.*)/lua(.*)$")
-  M.notifications = {}
   M.win_title = "NSpect 🧪"
   M.win_title_state = "Idle"
-  M.run_spec_count = nil
   M.previous_command = nil
-  M.error_data = ""
+  M.reset_state()
 
   vim.keymap.set("n", "<leader>R", M.reload_plugin)
   vim.keymap.set("n", "<leader>F", M.run_file)
@@ -50,21 +48,24 @@ M.create_run_buf = function()
   return bufnr, win
 end
 
-M.run_file = function()
+M.reset_state = function()
   M.notifications = {}
-  M.win_title_state = "Starting Up"
+  M.win_title_state = "Executing"
   M.run_spec_count = nil
   M.error_data = ""
+end
 
-  local filepath = vim.api.nvim_buf_get_name(0)
-  if not filepath:match(".*_spec%.rb") then
-    return
-  end
-  local bufnr, win = M.create_run_buf()
-  local use_bundle = vim.fn.filereadable(vim.fn.getcwd().."/".."Gemfile") == 1
+M.build_command = function(type, filepath, line_number)
   local cmd
+  local use_bundle = vim.fn.filereadable(vim.fn.getcwd().."/".."Gemfile") == 1
+  local command_file_part = filepath
+
+  if type == "line" then
+    command_file_part = command_file_part .. ":" .. line_number
+  end
+
   local cmd_args = {
-    filepath,
+    command_file_part,
     "--require",
     M.plugin_root .. "/rspec_formatter/jsonl_formatter.rb",
     "--format",
@@ -78,142 +79,61 @@ M.run_file = function()
   else
     cmd = "rspec"
   end
+
+  return cmd, cmd_args
+end
+
+M.run_file = function()
+  M.reset_state()
+
+  local filepath = vim.api.nvim_buf_get_name(0)
+  if not filepath:match(".*_spec%.rb") then
+    return
+  end
+  local cmd, cmd_args = M.build_command("file", filepath, nil)
+
   M.previous_command = {
     cmd = cmd,
     cmd_args = cmd_args,
   }
 
-  local stdout = vim.uv.new_pipe()
-  local stderr = vim.uv.new_pipe()
-  vim.uv.spawn(cmd, {
-    args = cmd_args,
-    stdio = { nil, stdout, stderr }
-  }, function()
-    vim.schedule(function()
-      if M.error_data == "" then
-        M.win_title_state = "Ran " .. M.run_spec_count .. " specs"
-      else
-        M.win_title_state = "Failed to run"
-      end
-      M.redraw_buff(bufnr, win)
-    end)
-  end)
-
-  vim.uv.read_start(stdout, function(_err, data)
-    if not data then return end
-
-    vim.schedule(function()
-      local notifications = RSpecParser.parse(data)
-      for _, notification in ipairs(notifications) do
-        if notification.type == "start" then
-          M.run_spec_count = notification.spec_count
-          M.win_title_state = "Running " .. M.run_spec_count .. " specs"
-        end
-        table.insert(M.notifications, notification)
-      end
-      M.redraw_buff(bufnr, win)
-    end)
-  end)
-
-  vim.uv.read_start(stderr, function(_err, data)
-    if not data then return end
-    M.error_data = M.error_data .. data
-
-    vim.schedule(function()
-      M.redraw_buff(bufnr, win)
-    end)
-  end)
+  M.execute_run()
 end
 
 M.run_line = function()
-  M.notifications = {}
-  M.win_title_state = "Starting Up"
-  M.run_spec_count = nil
-  M.error_data = ""
+  M.reset_state()
 
   local filepath = vim.api.nvim_buf_get_name(0)
   if not filepath:match(".*_spec%.rb") then
     return
   end
   local line_number = vim.api.nvim_win_get_cursor(0)[1]
-  local bufnr, win = M.create_run_buf()
-  local use_bundle = vim.fn.filereadable(vim.fn.getcwd().."/".."Gemfile") == 1
-  local cmd
-  local cmd_args = {
-    filepath .. ":" .. line_number,
-    "--require",
-    M.plugin_root .. "/rspec_formatter/jsonl_formatter.rb",
-    "--format",
-    "NSpect::JSONLFormatter",
-  }
+  local cmd, cmd_args = M.build_command("line", filepath, line_number)
 
-  if use_bundle then
-    cmd = "bundle"
-    table.insert(cmd_args, 1, "rspec")
-    table.insert(cmd_args, 1, "exec")
-  else
-    cmd = "rspec"
-  end
   M.previous_command = {
     cmd = cmd,
     cmd_args = cmd_args,
   }
-  local stdout = vim.uv.new_pipe()
-  local stderr = vim.uv.new_pipe()
-  vim.uv.spawn(cmd, {
-    args = cmd_args,
-    stdio = { nil, stdout, stderr }
-  }, function()
-    vim.schedule(function()
-      if M.error_data == "" then
-        M.win_title_state = "Ran " .. M.run_spec_count .. " specs"
-      else
-        M.win_title_state = "Failed to run"
-      end
-      M.redraw_buff(bufnr, win)
-    end)
-  end)
 
-  vim.uv.read_start(stdout, function(_err, data)
-    if not data then return end
-
-    vim.schedule(function()
-      local notifications = RSpecParser.parse(data)
-      for _, notification in ipairs(notifications) do
-        if notification.type == "start" then
-          M.run_spec_count = notification.spec_count
-          M.win_title_state = "Running " .. M.run_spec_count .. " specs"
-        end
-        table.insert(M.notifications, notification)
-      end
-      M.redraw_buff(bufnr, win)
-    end)
-  end)
-
-  vim.uv.read_start(stderr, function(_err, data)
-    if not data then return end
-    M.error_data = M.error_data .. data
-
-    vim.schedule(function()
-      M.redraw_buff(bufnr, win)
-    end)
-  end)
+  M.execute_run()
 end
 
 M.run_previous = function()
   if not M.previous_command then return end
 
-  M.notifications = {}
-  M.win_title_state = "Starting Up"
-  M.run_spec_count = nil
-  M.error_data = ""
+  M.reset_state()
+  M.execute_run()
+end
 
+M.execute_run = function()
   local bufnr, win = M.create_run_buf()
+  local cmd = M.previous_command.cmd
+  local cmd_args = M.previous_command.cmd_args
 
   local stdout = vim.uv.new_pipe()
   local stderr = vim.uv.new_pipe()
-  vim.uv.spawn(M.previous_command.cmd, {
-    args = M.previous_command.cmd_args,
+  vim.uv.spawn(cmd, {
+    args = cmd_args,
     stdio = { nil, stdout, stderr }
   }, function()
     vim.schedule(function()
